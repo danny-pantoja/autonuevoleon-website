ae0dfb39d396b1f372e9bb47659fc5b052d65348
danny-pantoja <40653531+danny-pantoja@users.noreply.github.com>
2025-05-23 14:28:21 -0500

    Update Cart Messages

--- autonuevoleon/cart/views.py
+++ autonuevoleon/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 def cart_summary(request):
     # Get the cart
@@ -31,9 +32,8 @@ def cart_add(request):
         # Return response
         # response = JsonResponse({ 'Product Name: ': product.name })
         response = JsonResponse({'qty': cart_quantity})
-
+        messages.success(request, ("Product Added to Cart.."))
         return response
-
 
 
 def cart_delete(request):
@@ -45,6 +45,7 @@ def cart_delete(request):
         cart.delete(product=product_id)
         
         response = JsonResponse({'product':product_id})
+        messages.success(request, ("Item has been removed from your cart.."))
         return response
 
 def cart_update(request):
@@ -57,5 +58,6 @@ def cart_update(request):
         cart.update(product=product_id, quantity=product_qty)
 
         response = JsonResponse({'qty':product_qty})
+        messages.success(request, ("Your Cart Has Been updated.."))
         return response
         #return redirect('cart_summary')
