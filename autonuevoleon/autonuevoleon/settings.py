e0dbfa387b796782e2d54e69948968f793844e5e
danny-pantoja <40653531+danny-pantoja@users.noreply.github.com>
2025-12-20 18:38:01 -0600

    Ready to deploy v6

--- autonuevoleon/autonuevoleon/settings.py
+++ autonuevoleon/autonuevoleon/settings.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 # Load Environmental Variables
 load_dotenv()
 
+# password DB
+DB_PASSWORD_YO = os.environ['DB_PASSWORD_YO']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ['DB_PASSWORD_YO'],
+        'PASSWORD': DB_PASSWORD_YO,
         'HOST': 'metro.proxy.rlwy.net',
         'PORT': '34418',
     }
