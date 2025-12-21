34547a3fea74775e20c080d76ec50dcabab676dc
danny-pantoja <40653531+danny-pantoja@users.noreply.github.com>
2025-12-21 14:03:20 -0600

    Ready to deploy v8

--- autonuevoleon/autonuevoleon/settings.py
+++ autonuevoleon/autonuevoleon/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static']
+STATICFILES_DIRS = ['static/']
 
 # Whitenoise Static Stuff
 
