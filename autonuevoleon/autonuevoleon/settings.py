0c559a4efea39c89084ccd6fba1f0348d9bf240b
danny-pantoja <40653531+danny-pantoja@users.noreply.github.com>
2025-12-21 14:34:22 -0600

    Ready to deploy v010

--- autonuevoleon/autonuevoleon/settings.py
+++ autonuevoleon/autonuevoleon/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DB_SECRET_PW']
+SECRET_KEY = 'django-insecure-#s%l#!6g#&=ml6@i@k88tfwzc4v28z57v%u4ml#lw5ngo#1(=1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
